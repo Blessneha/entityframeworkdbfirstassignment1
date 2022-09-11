d2500a72db8dd9f0745ecc7e0237515820de7f85
Blessneha <91933800+Blessneha@users.noreply.github.com>
2022-09-12 01:38:37 +0530

    Add files via upload

---
+++ ConsoleApp10/ConsoleApp10/ProductRepository.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class ProductRepository
+    {
+        public assign2Entities1 assign2;
+
+        public ProductRepository()
+        {
+            assign2 = new assign2Entities1();
+        }
+
+
+        public List<product> GetAllProducts()
+        {
+            try
+            {
+                List<product> products = assign2.products.ToList(); 
+                return products;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public List<product>GetproductBypname(string name1)
+        {
+            try
+            {
+                List<product> pro = assign2.products.Where(p => p.pname == name1).ToList();
+                return pro;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+
+        public product GetproductBypid(int id)
+        {
+            try
+            {
+                product p = assign2.products.Find(id);
+                return p;
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void AddProduct(product p)
+        {
+            try
+            {
+                assign2.products.Add(p); //Add Employee to EMployee table
+                assign2.SaveChanges(); //to save to record into the EMployee table
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void DeletProduct(int Id)
+        {
+            try
+            {
+                product pro = assign2.products.Find(Id);
+                assign2.products.Remove(pro); //Delete Employee from the table
+                assign2.SaveChanges(); //to save to record into the EMployee table
+
+
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        public void EditProduct(product newProduct)
+        {
+            try
+            {
+                product Oldpro = assign2.products.Find(newProduct.pid);
+                Oldpro.pprice=newProduct.pprice;
+                Oldpro.pname=newProduct.pname;
+                
+                assign2.SaveChanges(); //to save to record into the EMployee table
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ ConsoleApp10/ConsoleApp10/Program.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            ProductRepository productrepos = new ProductRepository();
+            do
+            {
+                Console.WriteLine("1.GetAllproducts\n2.Getproductbyname\n3.GetproductById\n4.Addproduct\n5.DeleteProduct\n6.EditProduct");
+                Console.WriteLine("Enter your choice");
+                int ch = int.Parse(Console.ReadLine());
+                switch (ch)
+                {
+                    case 1:
+                        {
+                            List<product> pro = productrepos.GetAllProducts();
+                            foreach (var p in pro)
+                            {
+                                Console.WriteLine($"ID:{p.pid} Name:{p.pname} price:{p.pprice}");
+                            }
+                        }
+                        break;
+                    case 2:
+                        {
+                            Console.WriteLine("Enter product Name");
+                            string name = Console.ReadLine();
+                            List<product> pro = productrepos.GetproductBypname(name);
+                            foreach (var p in pro)
+                            {
+                                Console.WriteLine($"ID:{p.pid} Name:{p.pname} price:{p.pprice}");
+                            }
+                        }
+                        break;
+                    case 3:
+                        {
+                            Console.WriteLine("Enter productId");
+                            int proId = int.Parse(Console.ReadLine());
+                            product pro = productrepos.GetproductBypid(proId);
+                            
+                            if (pro != null)
+                            {
+                                Console.WriteLine($"ID:{pro.pid} Name:{pro.pname} price:{pro.pprice}");
+                            }
+                            else
+                                Console.WriteLine("Invalid Id");
+                        }
+                        break;
+
+                    case 4:
+                        {
+                            product pro = new product();
+                            Console.WriteLine("Enter productId");
+                            pro.pid = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter product name");
+                            pro.pname = Console.ReadLine();
+
+                            Console.WriteLine("Enter price");
+                            pro.pprice = int.Parse(Console.ReadLine());
+                            productrepos.AddProduct(pro);
+
+                        }
+                        break;
+                    case 5:
+                        {
+                            Console.WriteLine("Enter Id");
+                            int Id = int.Parse(Console.ReadLine());
+                            productrepos.DeletProduct(Id);
+                        }
+                        break;
+                    case 6:
+                        {
+                            product pro = new product();
+                            Console.WriteLine("Enter productId");
+                            pro.pid = int.Parse(Console.ReadLine());
+                            Console.WriteLine("Enter product name");
+                            pro.pname = Console.ReadLine();
+                            
+                            Console.WriteLine("Enter price");
+                            pro.pprice = int.Parse(Console.ReadLine());
+
+                            productrepos.EditProduct(pro);
+
+                        }
+                        break;
+
+                }
+            } while (true);
+        }
+    }
+}
